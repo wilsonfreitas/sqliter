bcfa42f7c76d09e058863f7823894ddbd4746fbd
wilsonfreitas <wilson.freitas@gmail.com>
2014-05-25 20:12:13 -0300

    New tests implemented

--- inst/tests/test-sqliter.R
+++ inst/tests/test-sqliter.R
@@ -6,6 +6,23 @@ test_that('it should create sqliter', {
 	expect_equal(DBM$get('path'), '.')
 })
 
+context('database functions')
+
+test_that('it should list tables in each database', {
+	DBM <- sqliter(path='.')
+	res <- execute(DBM, 'funds', "select tbl_name from sqlite_master where type = 'table'")
+})
+
+test_that('it should list databases', {
+	DBM <- sqliter(path='.')
+	expect_equal(list_databases(DBM), c('funds', 'test'))
+})
+
+test_that('it should list databases using filter', {
+	DBM <- sqliter(path='.')
+	expect_equal(list_databases(DBM, 'fu'), 'funds')
+})
+
 context('execute query')
 
 test_that('it should execute a query', {
@@ -13,4 +30,5 @@ test_that('it should execute a query', {
 	res <- execute(DBM, 'funds', 'select count(*) from sqlite_master')
 	expect_equal(as.numeric(unlist(res)), 40)
 })
+
 
